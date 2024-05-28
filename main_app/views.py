54f18698cef8a8967d532470bba8ad4fd35e4839
VSoups <vince.kubota@gmail.com>
2024-05-28 14:29:42 -0700

    Part 1 base content

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+soups = [
+    {'name': 'Clam Chowder', 'Origin': 'Boudin SF', 'ingredients': ['Clams', 'Potatoes', 'Sourdough']},
+    {'name': 'Pho', 'Origin': 'Homemade', 'ingredients': ['Flank Steak', 'Rice Noodles', 'Bean Sprouts']},
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def soup_index(request):
+    return render(request, 'soups/index.html', {
+        'soups': soups,
+    })
